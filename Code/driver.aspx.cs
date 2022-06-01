a1fe5becf6678b70abf71239ccc24f417a53d5ae
bilal fayaz <bilalfayaz2016@gmail.com>
2022-06-01 23:52:53 +0500

    adding source code

---
+++ Code/driver.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CS370_project
+{
+    public partial class driver : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-RSFTGII;Initial Catalog=Ambulance;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                GVBind();
+            }
+        }
+        protected void GVBind()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select [BookingID], [Customer Name], [Phone Number], [Price (PKR)], Address, Time, [Ambulance Type] from Booking WHERE Completed = 0", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows == true)
+            {
+                Requests.DataSource = dr;
+                Requests.DataBind();
+
+            }
+            con.Close();
+
+        }
+        protected void ChkEmpty_CheckedChanged(object sender, EventArgs e)
+        {
+            con.Open();
+            // set Completed to 1 for that row of the table 
+            for (int i = 0; i < Requests.Rows.Count; i++)
+            {
+             
+
+                CheckBox chkTick = (CheckBox)Requests.Rows[i].Cells[0].FindControl("chkEmpty");
+                if (chkTick.Checked)
+                {
+                   
+                    int id = Convert.ToInt32(Requests.Rows[i].Cells[1].Text);
+                    
+                    SqlCommand cmd = new SqlCommand("UPDATE Booking SET Completed = 1 WHERE BookingId = '" + id + "'", con);
+                    cmd.ExecuteNonQuery();
+                    Requests.EditIndex = -1;
+                    con.Close();
+                }
+
+            }
+            GVBind();
+
+        }
+
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Solace : CS353
+
+## Technologies & Tools
+![](https://img.shields.io/badge/Editor-VisualStudio-informational?style=flat&logo=intellij-idea&logoColor=white&color=9f94d1)
+![](https://img.shields.io/badge/Framework-.NET-informational?style=flat&logo=C++&logoColor=white&color=9f94d1)
+![](https://img.shields.io/badge/Code-Python-informational?style=flat&logo=python&logoColor=white&color=9f94d1)
+![](https://img.shields.io/badge/Code-JavaScript-informational?style=flat&logo=javascript&logoColor=white&color=9f94d1)
+![](https://img.shields.io/badge/Code-Html-informational?style=flat&logo=HTML&logoColor=white&color=9f94d1)
+![](https://img.shields.io/badge/Code-CSS-informational?style=flat&logo=CSS&logoColor=white&color=9f94d1)
+![](https://img.shields.io/badge/Code-SQL-informational?style=flat&logo=C++&logoColor=white&color=9f94d1)
+![](https://img.shields.io/badge/Software-Jira-informational?style=flat&logo=C++&logoColor=white&color=9f94d1)
+
+
+##  Description
+
+ Solace is a web application that will simplify & streamline the process of seeking medical information and calling ambulances by efficiently reducing the time taken to call ambulances and providing adequate information of the whereabouts under medical encumbrance. 
+
+####  Drive Link - Sprint Meetings:
+https://drive.google.com/drive/folders/1tTDJmszZdrQ51u7eh6b0A_YOCJjqP8h_?usp=sharing
+
+##### &#128101; Group Members
+Aliza Saleem Lakhani (al05435) <br>
+Hafsa Irfan (hi05946)     <br>
+Mohammad Bilal Fayaz (mf05942)  <br>
+Sudais Baig (sb05588)           <br>
+
+
+
+
+ <!-- <details>
+  <summary>Homeworks</summary>
+   
+</details> -->
